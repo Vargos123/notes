7a698c54f3828930dd5a938bcf5b1a2fceae20f9
Vargos123 <BacaHoriyk@gmail.com>
2020-09-28 23:07:13 +0300

    all

--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,15 +9,24 @@ namespace WindowsFormsApp2
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        //appGuid для Mutex-a
+        private static string appGuid = "FB6B42D0-10D9-4563-AFF9-7123317FA272";
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            //Mutex для запрета повторного запуска приложения
+            using (Mutex mutex = new Mutex(false, @"Global\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Программа уже запущена. Повторный запуск программы невозможен", "Внимение:");
+                    return;
+                }
+                GC.Collect();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new LoginForm());
+            }
         }
     }
 }
